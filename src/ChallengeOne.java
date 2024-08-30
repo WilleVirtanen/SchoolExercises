843293696341b218b351b4bf68cc2e180793555d
WilleVirtanen <virtanen.wille@gmail.com>
2024-08-30 14:06:29 +0200

    first commit

---
+++ src/ChallengeOne.java
@@ -0,0 +1,20 @@
+public class ChallengeOne {
+public static void main(String[] args) {
+            int x=0;
+
+        if(x==0)
+        {
+            System.out.println("number is zero");
+        }
+        else if(x>=1)
+        {
+            System.out.println("number is posetive");
+        }
+        else 
+        {
+            System.out.println("number is negative");
+        }
+    }
+}
+
+
